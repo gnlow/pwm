7176de2a58289cfc1d973b82c82a2a82635c1c4c
Gnlow <gnlow@protonmail.com>
2024-02-18 18:58:35 +0900

    waveform visualizer

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,40 @@ const button = $.button({
     ]
 })
 
+const path = document.querySelector("path")!
+
 button.addEventListener("click", () => {
     const audioContext = new AudioContext()
 
     const mainGainNode = audioContext.createGain()
 
     mainGainNode.connect(audioContext.destination)
-    mainGainNode.gain.value = 0.2
+    mainGainNode.gain.value = 0.05
+
+    const analyser = audioContext.createAnalyser()
+    analyser.connect(mainGainNode)
+    analyser.fftSize = 2048
+    const bufferLength = analyser.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+
+    const f = () => {
+        analyser.getByteTimeDomainData(dataArray)
+
+        let result = ""
+        dataArray.forEach((v, i) => {
+            result += `L${i / dataArray.length * 500 -10} ${v / 256 * 100 + 50}`
+        })
+        path.setAttribute("d", "M -10 100" + result)
+
+        requestAnimationFrame(f)
+    }
+    f()
 
     const osc = audioContext.createOscillator()
-    osc.connect(mainGainNode)
+    osc.connect(analyser)
     
-    osc.type = "sine"
-    osc.frequency.value = 440
+    osc.type = "square"
+    osc.frequency.value = 128
     osc.start()
 })
 
