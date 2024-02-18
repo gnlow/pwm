3c8ad20daed782515fec9cf11a843a7a19234d7a
Gnlow <gnlow@protonmail.com>
2024-02-18 21:34:33 +0900

    what

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const button = $.button({
     ]
 })
 
+const svg = document.querySelector("svg")!
 const path = document.querySelector("path")!
 
 button.addEventListener("click", () => {
@@ -18,29 +19,39 @@ button.addEventListener("click", () => {
 
     const analyser = audioContext.createAnalyser()
     analyser.connect(mainGainNode)
-    analyser.fftSize = 2048
+    analyser.fftSize = 1024
     const bufferLength = analyser.frequencyBinCount
     const dataArray = new Uint8Array(bufferLength)
 
-    const f = () => {
-        analyser.getByteTimeDomainData(dataArray)
-
-        let result = ""
-        dataArray.forEach((v, i) => {
-            result += `L${i / dataArray.length * 500 -10} ${v / 256 * 100 + 50}`
-        })
-        path.setAttribute("d", "M -10 100" + result)
-
-        requestAnimationFrame(f)
-    }
-    f()
+    const timePerFrame = audioContext.sampleRate / analyser.fftSize
 
     const osc = audioContext.createOscillator()
     osc.connect(analyser)
     
-    osc.type = "square"
-    osc.frequency.value = 128
+    osc.type = "sine"
+    osc.frequency.value = 1000/12
     osc.start()
+
+    const f = (time: number) => () => {
+        analyser.getByteTimeDomainData(dataArray)
+        
+        const dt = Date.now() - time
+        console.log(dt)
+        osc.frequency.value = 2000/(dt || 1)
+
+        let result = ""
+        dataArray.forEach((v, i) => {
+            result += `
+                L
+                ${(i + dt / timePerFrame) / dataArray.length * svg.clientWidth}
+                ${v / 256 * 100 + 50}
+            `
+        })
+        path.setAttribute("d", "M -10 100" + result)
+
+        requestAnimationFrame(f(Date.now()))
+    }
+    requestAnimationFrame(f(Date.now()))
 })
 
 document.body.append(
